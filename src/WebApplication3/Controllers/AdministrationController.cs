4c90fa44203d4a69a858bdb7a4d076967e8afa83
Indrajit Bhattacharya <emailindra04@gmail.com>
2020-05-13 12:28:29 +0530

    creating roles 13/05/2020

---
+++ src/WebApplication3/Controllers/AdministrationController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.ViewModels;
+
+namespace WebApplication3.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel createRoleViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = createRoleViewModel.Role
+                };
+
+                IdentityResult identityResult=  await roleManager.CreateAsync(identityRole);
+
+                if(identityResult.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach(IdentityError error in identityResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(createRoleViewModel);
+        }
+
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var Role = roleManager.Roles;
+
+            return View(Role);
+        }
+
+    }
+}
